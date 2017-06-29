7b21e65cab91ffef3ec32c1666235310af97817c
anonymframework <vahit.serif119@gmail.com>
2017-06-29 16:30:35 +0300

    made some updates

--- src/Cable/Components/Rules/Alpha.php
+++ src/Cable/Components/Rules/Alpha.php
@@ -5,6 +5,10 @@ namespace Cable\Validation\Rules;
 
 use Cable\Validation\Rule;
 
+/**
+ * Class Alpha
+ * @package Cable\Validation\Rules
+ */
 class Alpha extends Rule
 {
 
@@ -25,6 +29,6 @@ class Alpha extends Rule
      */
     public function handle($data)
     {
-        return (preg_match("#^[a-zA-ZÀ-ÿ]+$#", $data) === 1);
+        return (preg_match('#^[a-zA-ZÀ-ÿ]+$#', $data) === 1);
     }
 }
